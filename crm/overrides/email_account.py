e15b5e11d2b881a66b4fe4477f59d01bb55b4910
Nidheesh P <nidheeshdevops@gmail.com>
2025-08-22 04:28:14 +0530

    Do not notify owner about inbound communication

--- crm/overrides/email_account.py
+++ crm/overrides/email_account.py
@@ -1,6 +1,7 @@
 import frappe
 from frappe import _
-from frappe.email.doctype.email_account.email_account import EmailAccount, InboundMail
+from frappe.email.receive import InboundMail, SentEmailInInboxError
+from frappe.email.doctype.email_account.email_account import EmailAccount
 
 # Custom get_inbound_mails method(frappe.email.doctype.email_account.email_account.EmailAccount.get_inbound_mails)
 def custom_get_inbound_mails(self) -> list[InboundMail]:
@@ -45,4 +46,39 @@ def custom_get_inbound_mails(self) -> list[InboundMail]:
 
     return mails
 
+# Custom receive method(frappe.email.doctype.email_account.email_account.EmailAccount.receive)
+def custom_receive(self):
+    """Called by scheduler to receive emails from this EMail account using POP3/IMAP."""
+    exceptions = []
+    inbound_mails = self.get_inbound_mails()
+    for mail in inbound_mails:
+        try:
+            communication = mail.process()
+            frappe.db.commit()
+            if communication and mail.flags.is_new_communication:
+                if self.enable_auto_reply:
+                    self.send_auto_reply(communication, mail)
+
+                # Skip sending outbound notification email
+                # communication.send_email(is_inbound_mail_communcation=True)
+        except SentEmailInInboxError:
+            frappe.db.rollback()
+        except Exception:
+            frappe.db.rollback()
+            try:
+                self.log_error(title="EmailAccount.receive")
+                if self.use_imap:
+                    self.handle_bad_emails(mail.uid, mail.raw_message, frappe.get_traceback())
+                exceptions.append(frappe.get_traceback())
+            except Exception:
+                frappe.db.rollback()
+            else:
+                frappe.db.commit()
+        else:
+            frappe.db.commit()
+
+    if exceptions:
+        raise Exception(frappe.as_json(exceptions))
+
 EmailAccount.get_inbound_mails = custom_get_inbound_mails
+EmailAccount.receive = custom_receive
